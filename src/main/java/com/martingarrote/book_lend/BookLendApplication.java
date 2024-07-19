3f487ac51512d9861cc465a4cab133b4f4427f6a
martingarrote <martindambrowskigarrote@gmail.com>
2024-07-19 11:50:32 -0300

    first commit

---
+++ src/main/java/com/martingarrote/book_lend/BookLendApplication.java
@@ -0,0 +1,13 @@
+package com.martingarrote.book_lend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookLendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookLendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/martingarrote/book_lend/BookLendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.martingarrote.book_lend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookLendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
