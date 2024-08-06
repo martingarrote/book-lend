9f3ebd7022c9f6fe8b4100a62b261c4a2cd9cc33
martingarrote <martindambrowskigarrote@gmail.com>
2024-08-06 08:12:40 -0300

    Implementar entidade Book

---
+++ src/main/java/com/martingarrote/book_lend/book/Book.java
@@ -0,0 +1,33 @@
+package com.martingarrote.book_lend.book;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "books")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String author;
+
+    @Column(nullable = false, unique = true)
+    private String isbn;
+
+    @Column(nullable = false)
+    private boolean available;
+
+}
