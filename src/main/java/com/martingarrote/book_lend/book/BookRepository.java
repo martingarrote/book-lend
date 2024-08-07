eab8a5816ff24c0d6b3205e950ad05edcd2173f8
martingarrote <martindambrowskigarrote@gmail.com>
2024-08-07 18:20:05 -0300

    Adicionar método para verificar livro por ISBN no BookRepository

--- src/main/java/com/martingarrote/book_lend/book/BookRepository.java
+++ src/main/java/com/martingarrote/book_lend/book/BookRepository.java
@@ -3,4 +3,5 @@ package com.martingarrote.book_lend.book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
+    boolean existsByIsbn(String isbn);
 }
