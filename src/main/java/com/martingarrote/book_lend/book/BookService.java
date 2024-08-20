188dfb6e2b2f84fdb25de851b0f802ea013dfcaf
martingarrote <martindambrowskigarrote@gmail.com>
2024-08-20 10:28:37 -0300

    Implementar métodos findAll e findById no BookService

--- src/main/java/com/martingarrote/book_lend/book/BookService.java
+++ src/main/java/com/martingarrote/book_lend/book/BookService.java
@@ -5,6 +5,9 @@ import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 @AllArgsConstructor
 public class BookService {
@@ -22,6 +25,17 @@ public class BookService {
         }
 
         return mapper.toDTO(repository.save(entity));
+    }
+
+    private List<BookDTO> findAll() {
+        return repository.findAll().stream().map(mapper::toDTO).toList();
+    }
+
+    private BookDTO findById(Long id) {
+        return repository.findById(id)
+                .map(mapper::toDTO)
+                // will be changed to custom exception
+                .orElseThrow(RuntimeException::new);
     }
 
 }
