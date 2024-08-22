0c747d391361754a0ff39fd239c38e4f543e0d75
martingarrote <martindambrowskigarrote@gmail.com>
2024-08-22 20:16:58 -0300

    Adicionar mapeamento de BookUpdateDTO para BookDTO

--- src/main/java/com/martingarrote/book_lend/mapper/BookMapper.java
+++ src/main/java/com/martingarrote/book_lend/mapper/BookMapper.java
@@ -2,6 +2,7 @@ package com.martingarrote.book_lend.mapper;
 
 import com.martingarrote.book_lend.book.Book;
 import com.martingarrote.book_lend.book.dto.BookDTO;
+import com.martingarrote.book_lend.book.dto.BookUpdateDTO;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
@@ -10,6 +11,7 @@ public interface BookMapper {
     BookMapper INSTANCE = Mappers.getMapper(BookMapper.class);
 
     BookDTO toDTO(Book book);
+    BookDTO toDTO(BookUpdateDTO updateDTO);
 
     Book toEntity(BookDTO bookDTO);
 }
