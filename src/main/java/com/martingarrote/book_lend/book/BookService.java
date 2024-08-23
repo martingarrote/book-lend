b4bfa11a37d988acf240c191e6c6103872bd2fb9
martingarrote <martindambrowskigarrote@gmail.com>
2024-08-22 22:13:35 -0300

    Adaptar método privado save para operações PUT e PATCH

--- src/main/java/com/martingarrote/book_lend/book/BookService.java
+++ src/main/java/com/martingarrote/book_lend/book/BookService.java
@@ -1,9 +1,11 @@
 package com.martingarrote.book_lend.book;
 
 import com.martingarrote.book_lend.book.dto.BookDTO;
+import com.martingarrote.book_lend.book.dto.BookUpdateDTO;
 import com.martingarrote.book_lend.mapper.BookMapper;
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,14 +18,14 @@ public class BookService {
     private final BookMapper mapper = BookMapper.INSTANCE;
 
     @Transactional
-    private BookDTO save(BookDTO dto, boolean isNew) {
+    private BookDTO save(BookDTO dto, boolean isNew, boolean isPatch) {
         Book entity = mapper.toEntity(dto);
 
         if (isNew) {
             entity.setAvailable(true);
         }
 
-        if (repository.existsByIsbn(entity.getIsbn())) {
+        if (repository.existsByIsbn(entity.getIsbn()) && (isNew || isPatch)) {
             // custom exceptions and handlers will be added later
             throw new RuntimeException();
         }
@@ -32,7 +34,7 @@ public class BookService {
     }
 
     public BookDTO create(BookDTO dto) {
-        return save(dto, true);
+        return save(dto, true, false);
     }
 
     public List<BookDTO> findAll() {
