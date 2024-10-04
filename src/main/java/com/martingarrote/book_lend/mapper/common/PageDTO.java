67735c7b6b99970f96eb7b415eec99c5656f5205
martingarrote <martindambrowskigarrote@gmail.com>
2024-10-04 02:33:40 -0300

    Corrigir e adicionar campos em PageDTO

--- src/main/java/com/martingarrote/book_lend/mapper/common/PageDTO.java
+++ src/main/java/com/martingarrote/book_lend/mapper/common/PageDTO.java
@@ -3,8 +3,14 @@ package com.martingarrote.book_lend.mapper.common;
 import java.util.List;
 
 public record PageDTO<T>(
+
         List<T> data,
+
         int page,
+
         int size,
-        int totalElements
+
+        long totalElements,
+
+        long totalPages
 ) {}
